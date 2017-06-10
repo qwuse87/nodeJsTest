dab3041c4d7f0789f989d63af21b4ba2b0b31faa
Leesuyun <qwuse87@gmail.com>
2017-06-10 16:25:42 +0900

    사용자 정의 모듈 추가 샘플

---
+++ modules/combineJS.js
@@ -0,0 +1,9 @@
+/*! combineJS module @ qwuse.com, 2017*/
+
+// combineJS 모듈 정의
+function combineJS(){
+	console.log('combineJS 모듈');
+}
+
+// combineJS 모듈 외부로 출력(Exports)
+module.exports = combineJS;
